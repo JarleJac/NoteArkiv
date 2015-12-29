ee2b7b180b9531a62d25d7537fc1d4aa45f8c7a5
JarleJac <jarle.jacobsen@gmail.com>
2015-12-29 16:23:21 +0100

    Added routing using ngRoute and some structure to the webapp

--- jacJarSoft.noteArkiv.webapp/WebContent/script/notearkiv.js
+++ jacJarSoft.noteArkiv.webapp/WebContent/script/notearkiv.js
@@ -1,8 +1,21 @@
 (function() {
-  var app = angular.module('notearkiv', []);
+	angular.module('notearkiv', [ 'ngRoute' ])
+	.config(function($routeProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl : 'templates/pages/home/index.html'
+		})
+		.when('/sheets/new', {
+			templateUrl : 'templates/pages/sheets/sheet.html'
+		})
+		.when('/quicklists', {
+			templateUrl : 'templates/pages/quicklists/index.html'
+		})
+		;
+	})
+	.controller('someController', function() {
 
-  app.controller('someController', function(){
-    
-  });
+	})
+	;
 
 })();
