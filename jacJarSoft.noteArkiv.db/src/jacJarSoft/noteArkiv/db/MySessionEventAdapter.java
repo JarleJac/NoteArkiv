75471820a95691e9868bd9f180f0f288b5a006f5
JarleJac <jarle.jacobsen@gmail.com>
2017-02-06 19:59:32 +0100

    increase/set the wait time before SQLITE reports busy

--- jacJarSoft.noteArkiv.db/src/jacJarSoft/noteArkiv/db/MySessionEventAdapter.java
+++ jacJarSoft.noteArkiv.db/src/jacJarSoft/noteArkiv/db/MySessionEventAdapter.java
@@ -1,5 +1,6 @@
 package jacJarSoft.noteArkiv.db;
 
+import org.eclipse.persistence.sessions.Session;
 import org.eclipse.persistence.sessions.SessionEvent;
 import org.eclipse.persistence.sessions.SessionEventAdapter;
 
@@ -7,7 +8,9 @@ public class MySessionEventAdapter extends SessionEventAdapter {
 
 	@Override
 	public void postAcquireClientSession(SessionEvent event) {
-		event.getSession().executeNonSelectingSQL("PRAGMA foreign_keys=ON");
+		Session session = event.getSession();
+		session.executeNonSelectingSQL("PRAGMA foreign_keys=ON");
+		session.executeSQL("pragma busy_timeout=30000"); //Busy timeout set to 30000 milliseconds
 		super.postAcquireClientSession(event);
 	}
 }
