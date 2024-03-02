29634f006208330f0ed50b72238d7a9f1477099d
JarleJac <jarle.jacobsen@gmail.com>
2024-03-02 17:07:16 +0100

    Using setting or uriInfo to build base url for link in pw mail

--- jacJarSoft.noteArkiv.webapp/src/main/java/jacJarSoft/noteArkiv/internal/mail/ForgotPwMailSender.java
+++ jacJarSoft.noteArkiv.webapp/src/main/java/jacJarSoft/noteArkiv/internal/mail/ForgotPwMailSender.java
@@ -1,8 +1,6 @@
 package jacJarSoft.noteArkiv.internal.mail;
 
 import java.io.StringWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -18,19 +16,23 @@ public class ForgotPwMailSender {
     private static Logger logger = Logger.getLogger(ForgotPwMailSender.class.getName());
 
 	private User user;
-	private AppInfo appInfo;
-
+	private AppInfo appInfo = new AppInfo();
 	private Configuration configuration;
+	//private NoteArkivSettings noteArkivSettings;
+	private String baseNotearkivUrl;
 
-	public ForgotPwMailSender(User user, AppInfo appInfo, Configuration configuration) {
+	public ForgotPwMailSender(User user, NoteArkivSettings noteArkivSettings, Configuration configuration, String baseNotearkivUrl) {
 		this.user = user;
-		this.appInfo = appInfo;
+		//this.noteArkivSettings = noteArkivSettings;
 		this.configuration = configuration;
+		this.baseNotearkivUrl = baseNotearkivUrl;
+
+		appInfo.setAppSettings(noteArkivSettings.getJsAppSettings());
 	}
 
 	public void sendMail() {
 		logger.info("forgot email called for user " + user.getNo());
-		String htmlContent = builMailHtml();
+		String htmlContent = builMailHtml(buildUrl());
 		
 		String subject = "Nullstille passordet for din bruker til " + appInfo.getAppSettings().getApplicationTitle();
 		String recipientTO = "jarle.jacobsen@gmail.com";
@@ -41,13 +43,23 @@ public class ForgotPwMailSender {
 		
 	}
 
-	private String builMailHtml() {
+	private String buildUrl() {
+		String tokenString = buildToken();
+		String resetPwUrl = baseNotearkivUrl + "setPw?spts=" + tokenString;
+		return resetPwUrl;
+	}
+
+	private String buildToken() {
+		return "TODO";
+	}
+
+	private String builMailHtml(String resetPwUrl) {
 		String htmlContent;
 		try {
 			Map<String, Object> data = new HashMap<>();
 			data.put("user", user);
 			data.put("appInfo", appInfo);
-			data.put("resetPwUrl", "http:/something.com");
+			data.put("resetPwUrl", resetPwUrl);
 			Template template = configuration.getTemplate("forgotPw.html");
 			StringWriter writer = new StringWriter();
 			template.process(data, writer);
